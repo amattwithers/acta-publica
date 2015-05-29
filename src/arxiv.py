e12de7cc6d1816ccd2ef352be6dba50da2457a74
Matthew Withers <matthew.withers@port.ac.uk>
2015-05-29 21:38:57 +0100

    Implemented infintite scrolling feature.

--- src/arxiv.py
+++ src/arxiv.py
@@ -7,9 +7,9 @@ import feedparser
 
 class ItemPopup(QtGui.QWidget):
 
-    def __init__(self, parent=None):
+    def __init__(self, parent=QtCore.QCoreApplication.instance()):
 
-        super(ItemPopup, self).__init__()
+        super(ItemPopup, self).__init__(parent)
 
         self.setGeometry(0, 0, 1024, 576)
         # self.setFixedSize(1024, 576)
@@ -77,6 +77,8 @@ class ListItem(QtGui.QListWidgetItem):
 
         self.setSizeHint(QtCore.QSize(0, 30))
 
+        self.action = ''
+
         self.data = ''
 
 
@@ -90,9 +92,13 @@ class Arxiv(QtGui.QListWidget):
 
         self.data = ''
 
+        self.counter = 0
+
         self.setVerticalScrollMode(1)
 
     def Query(self, query):
+
+        self.counter += 1
 
         prefix = 'http://export.arxiv.org/api/query?'
 
@@ -152,6 +158,7 @@ class Arxiv(QtGui.QListWidget):
                     }
 
             listEntry = ListItem()
+            listEntry.action = 'popup'
             listEntry.data = data
             listEntry.setText(title)
             self.addItem(listEntry)
@@ -162,13 +169,85 @@ class Arxiv(QtGui.QListWidget):
         addMore.setTextAlignment(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignVCenter)
         # addMore.setBackground(QtCore.Qt.green)
         addMore.setBackground(QtGui.QColor(88, 214, 91, 255))
+        addMore.action = 'addmore'
         self.addItem(addMore)
 
     def showPopup(self):
 
-        self.popup = ItemPopup()
-        self.popup.setData(self.selectedItems()[0].data)
-        self.popup.show()
+        if self.selectedItems()[0].action == 'popup':
+
+            self.popup = ItemPopup()
+            self.popup.setData(self.selectedItems()[0].data)
+            self.popup.show()
+
+        elif self.selectedItems()[0].action == 'addmore':
+
+            self.selectedItems()[0].setHidden(True)
+
+            d = feedparser.parse(self.query + '&start=' + str(self.counter))
+            self.counter += 1
+
+            for entry in d.entries:
+
+                try:
+                    id = entry.id.replace("\n", " ").replace("\r", " ")
+                except AttributeError:
+                    id = "No id found!"
+
+                try:
+                    title = entry.title.replace("\n", " ").replace("\r", " ")
+                except AttributeError:
+                    title = "No Title Found!"
+
+                try:
+                    summary = entry.summary.replace("\n", " ").replace("\r", " ")
+                except AttributeError:
+                    summary = "No Summary Found!"
+
+                try:
+                    authstr = ', '.join(author.name for author in entry.authors)
+                    authstr = authstr.replace("\n", " ").replace("\r", " ")
+                except AttributeError:
+                    authstr = "No Authors Found!"
+
+                try:
+                    date = entry.updated.replace("\n", " ").replace("\r", " ")
+                except AttributeError:
+                    date = "No data Found!"
+
+                try:
+                    journ_ref = entry.arxiv_journal_ref.replace("\n", " ").replace("\r", " ")
+                except AttributeError:
+                    journ_ref = "No Journal Ref Found!"
+
+                try:
+                    comment = entry.arxiv_comment.replace("\n", " ").replace("\r", " ")
+                except AttributeError:
+                    comment = "No Comment Found!"
+
+                data = {'id': id,
+                        'title': title,
+                        'authors': authstr,
+                        'date': date,
+                        'journal': journ_ref,
+                        'comment': comment,
+                        'summary': summary
+                        }
+
+                listEntry = ListItem()
+                listEntry.action = 'popup'
+                listEntry.data = data
+                listEntry.setText(title)
+                self.addItem(listEntry)
+
+            addMore = ListItem()
+            addMore.setText("More")
+            addMore.setSizeHint(QtCore.QSize(0, 50))
+            addMore.setTextAlignment(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignVCenter)
+            # addMore.setBackground(QtCore.Qt.green)
+            addMore.setBackground(QtGui.QColor(88, 214, 91, 255))
+            addMore.action = 'addmore'
+            self.addItem(addMore)
 
 
 if __name__ == "__main__":
